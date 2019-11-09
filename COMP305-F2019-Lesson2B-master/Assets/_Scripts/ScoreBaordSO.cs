14662c9b5ccfde50d9b5b023b32fa46b031ee045
alhafeez1517 <55024380+alhafeez1517@users.noreply.github.com>
2019-11-09 09:49:19 -0500

    Added Project

---
+++ COMP305-F2019-Lesson2B-master/Assets/_Scripts/ScoreBaordSO.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName ="ScoreBoardSO",menuName ="Game/ScoreBoard")]
+[System.Serializable]
+public class ScoreBaordSO : ScriptableObject
+{
+    public int score;
+    public int lives;
+    public int highScore;
+}
